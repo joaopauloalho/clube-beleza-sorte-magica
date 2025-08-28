d2fd7a7264bcafc342440fa712dd9f2ce5ed1ec1
joaopauloalho <62901157+joaopauloalho@users.noreply.github.com>
2025-08-28 11:30:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/clube-beleza-sorte-magica/",  // 👈 ADICIONE ESTA LINHA
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
